d7e1966c8bbe4d58e17b762bfb08927a090b6fcb
Jane <117987532+Janeie@users.noreply.github.com>
2023-10-22 18:56:37 +0100

    Add files via upload

---
+++ RoomsFile.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotelproject;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author PC
+ */
+public class RoomsFile {
+    public static void rooms(){
+        try {
+      File myObj = new File("filename.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+    }
+
